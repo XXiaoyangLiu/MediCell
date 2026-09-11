355b5e8490c55c321e8cdd0a8053f75326ab40b4
XXiaoyangLiu <liuxiaoyang062525@163.com>
2026-09-11 17:20:51 +0800

    Update meta_analysis.sh

--- script/meta_analysis.sh
+++ script/meta_analysis.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-cd "/node1/liuxy/MediCell/kidney_3/glom/TWAS/usefile/"
+cd "/node1/liuxy/MediCell/kidney_3/glom/TWAS/usefile/GWAS/"
 
 #GCST011096
-cat "/node1/liuxy/check/other_SLE_GWAS/GCST011096/GCST011096_aligned.tsv" | awk '
+cat GCST011096_aligned.tsv | awk '
 BEGIN {OFS="\t"}
 NR == 1 {print "SNP", "A1", "A2", "FREQ", "BETA", "SE", "P", "N"}
 NR > 1 {
     print $9, $4, $5, "NA", $6, $7, $8, "25268"
 }' > GCST011096.txt
 #GCST90018917
-cat "/node1/liuxy/check/other_SLE_GWAS/GCST90018917/GCST90018917_aligned.tsv" | awk '
+cat GCST90018917_aligned.tsv | awk '
 BEGIN {OFS="\t"}
 NR == 1 {print "SNP", "A1", "A2", "FREQ", "BETA", "SE", "P", "N"}
 NR > 1 {
     print $9, $3, $4, $5, $6, $7, $8, "659165"
 }' > GCST90018917.txt
 #GCST90476183
-cat "/node1/liuxy/check/other_SLE_GWAS/GCST90476183/GCST90476183_aligned.tsv" | awk '
+cat GCST90476183_aligned.tsv | awk '
 BEGIN {OFS="\t"}
 NR == 1 {print "SNP", "A1", "A2", "FREQ", "BETA", "SE", "P", "N"}
 NR > 1 {
@@ -39,8 +39,8 @@ PROCESS GCST011096.txt
 PROCESS GCST90018917.txt
 PROCESS GCST90476183.txt
 
-OUTFILE SLE_metal_096_917_453 .tbl
-OUTFILE SLE_metal_096_917_453 .tbl
+OUTFILE SLE_metal_096_917_183 .tbl
+OUTFILE SLE_metal_096_917_183 .tbl
 ANALYZE HETEROGENEITY
 
 QUIT
