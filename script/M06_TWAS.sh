f8da8aaea68082da48a731824d235f2718039562
XXiaoyangLiu <liuxiaoyang062525@163.com>
2026-09-11 22:21:46 +0800

    Update M06_TWAS.sh

--- script/M06_TWAS.sh
+++ script/M06_TWAS.sh
@@ -1,59 +1,151 @@
 #!/bin/bash
-#scpredixcan
 source ~/.bashrc
 conda activate imlatools
-cd "/node1/liuxy/MediCell/pbmc/TWAS/output/TWAS_2/"
 declare -A celltypes=(
-    ["pbmc"]="CD4T CD8T NK B Mono DC"
-)
-declare -A tissue_paths=(
-    ["pbmc_db"]="/node1/liuxy/MediCell/pbmc/TWAS/usefile/db_2/"
-    ["pbmc_cov"]="/node1/liuxy/MediCell/pbmc/TWAS/usefile/cov_6/"
+    ["pbmc"]="B CD4T CD8T DC Mono NK"
 )
 disease_name="SLE"
-gwas_files=/node1/liuxy/MediCell/pbmc/TWAS/usefile/bentham_2015_26502338_sle_processed_beta.tsv
-output_base="/node1/liuxy/MediCell/pbmc/TWAS/output/TWAS_2/"
-for gwas_path in "${gwas_files[@]}"; do
-    disease_name=$(basename $(dirname "$gwas_path"))
-    disease_name="SLE"
-    for tissue in "${!celltypes[@]}"; do
-        tissue_output_dir="${output_base}/${tissue}/${disease_name}"
-        mkdir -p "$tissue_output_dir"
-        db_path="${tissue_paths[${tissue}_db]}"
-        cov_path="${tissue_paths[${tissue}_cov]}"
-        
-        for ct_value in ${celltypes[$tissue]}; do
-            output_file="${tissue_output_dir}/${ct_value}.csv"
-            log_file="${tissue_output_dir}/${ct_value}.log"
-            
-            if [[ "$tissue" == "pbmc" ]]; then
-                db_file="${db_path}/${ct_value}_PCs_and_age_gender_models_filtered_signif.db"
-            else
-                db_file="${db_path}/${ct_value}.db"
-            fi
-            cov_file="${cov_path}/${ct_value}.txt.gz"            
-            if [[ ! -f "$db_file" ]]; then
-                echo "DB file not found: $db_file" >&2
-                continue
-            fi
-            if [[ ! -f "$cov_file" ]]; then
-                echo "Covariance file not found: $cov_file" >&2
-                continue
-            fi
-            echo "Processing ${disease_name} - ${tissue} - ${ct_value}"
-            /public/home/liuxy/MetaXcan-master/software/SPrediXcan.py \
-                --model_db_path "$db_file" \
+gwas_files="/node1/liuxy/MediCell/pbmc_2/TWAS/usefile/GWAS/SLE_metal_096_917_183_filt.tsv"
+
+declare -A tissue_paths=(
+    ["pbmc_db"]="/node1/liuxy/MediCell/pbmc/TWAS/usefile/db_2/"
+    ["pbmc_cov"]="/node1/liuxy/MediCell/pbmc_2/TWAS/usefile/cov_EUR_EAS/"
+)
+output_base="/node1/liuxy/MediCell/pbmc_2/TWAS/output/"
+for tissue in "${!celltypes[@]}"; do
+    tissue_output_dir="${output_base}/${tissue}/${disease_name}"
+    mkdir -p "$tissue_output_dir"
+    db_path="${tissue_paths[${tissue}_db]}"
+    cov_path="${tissue_paths[${tissue}_cov]}"
+    for ct_value in ${celltypes[$tissue]}; do
+        output_file="${tissue_output_dir}/${ct_value}.csv"
+        log_file="${tissue_output_dir}/${ct_value}.log"
+        db_file="${db_path}/${ct_value}_PCs_and_age_gender_models_unfiltered.db"
+        cov_file="${cov_path}/${ct_value}.txt.gz"
+        if [[ ! -f "$db_file" ]]; then
+            echo "db not exist: $db_file" >&2
+            continue
+        fi
+        if [[ ! -f "$cov_file" ]]; then
+            echo "cov not exist: $cov_file" >&2
+            continue
+        fi
+        echo "${disease_name} | ${tissue} | ${ct_value}"
+        /public/home/liuxy/MetaXcan-master/software/SPrediXcan.py \
+            --model_db_path "$db_file" \
                 --covariance "$cov_file" \
                 --keep_non_rsid \
                 --model_db_snp_key VarID \
                 --gwas_file "$gwas_files" \
-                --snp_column VariantID \
-                --effect_allele_column effect_allele \
-                --non_effect_allele_column other_allele \
-                --zscore_column zscore \
-                --beta_column beta \
-                --pvalue_column P.value \
+                --snp_column MarkerName \
+                --effect_allele_column Allele1 \
+                --non_effect_allele_column Allele2 \
+                --beta_column Effect \
+                --pvalue_column P-value \
+                --se_column StdErr \
+                --input_pvalue_fix 0 \
                 --output_file "$output_file" > "$log_file" 2>&1
-        done
     done
-done 
+done
+
+
+
+#glom
+declare -A celltypes=(
+    ["glom"]="Endo Podo MC NK NKT Th_cell CD8T Bcell MNP"
+)
+disease_name="SLE"
+gwas_files="/node1/liuxy/check/other_SLE_GWAS/metal/20260830/SLE_metal_096_917_183_filt_3.tsv"
+
+declare -A tissue_paths=(
+    ["glom_db"]="/node1/liuxy/MediCell/kidney_3/glom/TWAS/usefile/db/"
+    ["glom_cov"]="/node1/liuxy/MediCell/kidney_3/glom/TWAS/usefile/cov_EUR_EAS/"
+)
+output_base="/node1/liuxy/MediCell/kidney_3/glom/TWAS/output/"
+for tissue in "${!celltypes[@]}"; do
+    tissue_output_dir="${output_base}/${tissue}/${disease_name}"
+    mkdir -p "$tissue_output_dir"
+    db_path="${tissue_paths[${tissue}_db]}"
+    cov_path="${tissue_paths[${tissue}_cov]}"
+    for ct_value in ${celltypes[$tissue]}; do
+        output_file="${tissue_output_dir}/${ct_value}.csv"
+        log_file="${tissue_output_dir}/${ct_value}.log"
+        db_file="${db_path}/${ct_value}_models_filtered_signif.db"
+        cov_file="${cov_path}/${ct_value}.txt.gz"
+        if [[ ! -f "$db_file" ]]; then
+            echo "db not exist: $db_file" >&2
+            continue
+        fi
+        if [[ ! -f "$cov_file" ]]; then
+            echo "cov not exist: $cov_file" >&2
+            continue
+        fi
+        echo "${disease_name} | ${tissue} | ${ct_value}"
+        /public/home/liuxy/MetaXcan-master/software/SPrediXcan.py \
+            --model_db_path "$db_file" \
+                --covariance "$cov_file" \
+                --keep_non_rsid \
+                --model_db_snp_key VarID \
+                --gwas_file "$gwas_files" \
+                --snp_column MarkerName \
+                --effect_allele_column Allele1 \
+                --non_effect_allele_column Allele2 \
+                --beta_column Effect \
+                --pvalue_column P-value \
+                --se_column StdErr \
+                --input_pvalue_fix 0 \
+                --output_file "$output_file" > "$log_file" 2>&1
+    done
+done
+
+#tub
+#!/bin/bash
+source ~/.bashrc
+conda activate imlatools
+declare -A celltypes=(
+    ["tub"]="Bcell CD8T CDICA CDICB DCT LOH MNP NK NKT PT Th_cell Plasmacytoid_DC"
+)
+disease_name="SLE"
+gwas_files="/node1/liuxy/check/other_SLE_GWAS/metal/20260830/SLE_metal_096_917_183_filt_3.tsv"
+
+declare -A tissue_paths=(
+    ["tub_db"]="/node1/liuxy/MediCell/kidney_3/tub/TWAS/usefile/db/"
+    ["tub_cov"]="/node1/liuxy/MediCell/kidney_3/tub/TWAS/usefile/cov_EUR_EAS/"
+)
+output_base="/node1/liuxy/MediCell/kidney_3/tub/TWAS/output/"
+for tissue in "${!celltypes[@]}"; do
+    tissue_output_dir="${output_base}/${tissue}/${disease_name}"
+    mkdir -p "$tissue_output_dir"
+    db_path="${tissue_paths[${tissue}_db]}"
+    cov_path="${tissue_paths[${tissue}_cov]}"
+    for ct_value in ${celltypes[$tissue]}; do
+        output_file="${tissue_output_dir}/${ct_value}.csv"
+        log_file="${tissue_output_dir}/${ct_value}.log"
+        db_file="${db_path}/${ct_value}_models_filtered_signif.db"
+        cov_file="${cov_path}/${ct_value}.txt.gz"
+        if [[ ! -f "$db_file" ]]; then
+            echo "db not exist: $db_file" >&2
+            continue
+        fi
+        if [[ ! -f "$cov_file" ]]; then
+            echo "cov not exist: $cov_file" >&2
+            continue
+        fi
+        echo "${disease_name} | ${tissue} | ${ct_value}"
+        /public/home/liuxy/MetaXcan-master/software/SPrediXcan.py \
+            --model_db_path "$db_file" \
+                --covariance "$cov_file" \
+                --keep_non_rsid \
+                --model_db_snp_key VarID \
+                --gwas_file "$gwas_files" \
+                --snp_column MarkerName \
+                --effect_allele_column Allele1 \
+                --non_effect_allele_column Allele2 \
+                --beta_column Effect \
+                --pvalue_column P-value \
+                --se_column StdErr \
+                --input_pvalue_fix 0 \
+                --output_file "$output_file" > "$log_file" 2>&1
+    done
+done
+
